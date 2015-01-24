4ae5c7658e34bb2459efcec1034bbc79678897cb
ermolushka <alexeyermolaev91@gmail.com>
2015-01-25 01:57:47 +0300

    Initial Commit

---
+++ LinguaCard/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LinguaCard
+//
+//  Created by alexey on 25.01.15.
+//  Copyright (c) 2015 alexey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
